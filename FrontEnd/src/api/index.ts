82aeb8823eb616784b4865a9f9c4a248fb61b65f
nohnohnohnoh <shduddhks1234@gmail.com>
2023-08-06 12:46:44 +0900

    modify : BASE_URL

--- FrontEnd/src/api/index.ts
+++ FrontEnd/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:80"; //"http://52.79.223.155/";
+const BASE_URL = "http://3.39.253.185/";
 
 const axiosApi = (url = BASE_URL) => {
   const instance = axios.create({ baseURL: url });
