1407aee6f62868208993ec029b145b72103c0ff7
nohnohnohnoh <shduddhks1234@gmail.com>
2023-09-13 12:13:26 +0900

    modify: BASE_URL

--- FrontEnd/src/api/index.ts
+++ FrontEnd/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001"; //"http://3.38.116.92:3001/"
+const BASE_URL = "http://13.125.244.187:3001/";
 
 const axiosApi = (url = BASE_URL) => {
   const instance = axios.create({ baseURL: url });
